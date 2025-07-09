21d9dacfb49557180ecb376f945265a8fa7d478e
Andrei Manea (Local Admin) <AndManAdmin@softwire.com>
2025-07-09 15:15:29 +0300

    connected db and listed all books

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,13 +1,27 @@
 import { Router, Request, Response } from 'express';
+import { getAllBooks } from '../services/bookService';
 
 class BookController {
     router: Router;
 
     constructor() {
         this.router = Router();
+
+        this.router.get('/', this.getAllBooks.bind(this));
+
         this.router.get('/:id', this.getBook.bind(this));
 
         this.router.post('/', this.createBook.bind(this));
+    }
+
+    async getAllBooks(req: Request, res: Response) {
+        try {
+            const books = await getAllBooks();
+            res.json(books);
+        } catch (error) {
+            console.error('Error fetching books:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
     }
 
     getBook(req: Request, res: Response) {
---
+++ src/services/bookService.ts
@@ -0,0 +1,12 @@
+// services/bookService.ts
+import poolPromise from '../db';
+import { Book } from '../models/book';
+
+export async function getAllBooks(): Promise<Book[]> {
+    const pool = await poolPromise;
+    const result = await pool.request().query('SELECT id, title, isbn FROM book');
+
+    return result.recordset.map(
+        row => new Book(row.id, row.title, row.isbn)
+    );
+}
